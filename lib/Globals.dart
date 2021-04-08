fc259c868239a68a016f4ef53436072a2a053cdf
YANIV MADMON <yaniv.madmon@live.biu.ac.il>
2021-04-08 11:13:09 +0300

    globals

--- lib/Globals.dart
+++ lib/Globals.dart
@@ -3,6 +3,7 @@ import 'package:havruta_project/DataBase_auth/mongo.dart';
 import 'DataBase_auth/User.dart';
 
 class Globals {
+
   static var db = new Mongo();
   static bool isDbConnect = false;
   static User currentUser;
