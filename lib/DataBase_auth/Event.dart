083b4af3d29f53840a29e851ebeb9794e8100f44
Yonatan Gat <4yonatan4@gmail.com>
2021-04-18 09:22:50 +0300

    Event update

--- lib/DataBase_auth/Event.dart
+++ lib/DataBase_auth/Event.dart
@@ -1,43 +1,47 @@
 // ------------------------------ Event CLASS ------------------------------
 
-
-
+import 'package:havruta_project/DataBase_auth/User.dart';
 
 class Event {
-  String _user, _type, _topic, _book, _times, _hour, _date, _participants, _link,
-      _descripton;
+  String _email,
+      _type,
+      _topic,
+      _book,
+      _frequency,
+      _date,
+      _link,
+      _description;
+  String eventImage;
+  int starRating;
+  final List<User> _participants;
 
   String get topic => _topic;
   String get book => _book;
 
-  Event( this._user, this._type, this._topic, this._book, this._times, this._hour,
-      this._date, this._participants, this._link, this._descripton);
+  Event( this._email, this._type, this._topic, this._book, this._date,
+      this._participants, this._link, this._description);
 
   // Return JSON of the event
   Map<String, dynamic> toJson() => {
-    'user': _user,
+    'email': _email,
     'type': _type,
     'topic': _topic,
     'book': _book,
-    'times': _times,
-    'hour': _hour,
+    'frequency': _frequency,
     'date': _date,
     'participants': _participants,
     'link': _link,
-    'descripton': _descripton
+    'description': _description
   };
 
   Event.fromJson(Map<String, dynamic> json)
-
       :
-        _user = json['user'],
+        _email = json['email'],
         _type = json['type'],
         _topic = json['topic'],
         _book = json['book'],
-        _times = json['times'],
-        _hour = json['hour'],
+        _frequency = json['frequency'],
         _date = json['date'],
         _participants = json['participants'],
-        _descripton = json['descripton'];
-
+        _description = json['description'];
 }
