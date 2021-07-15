40fc4a53a6da6889c00b83e72cc324e7f2c20f42
YANIV MADMON <yaniv.madmon@live.biu.ac.il>
2021-07-15 19:50:48 +0300

    feed

--- lib/main.dart
+++ lib/main.dart
@@ -92,8 +92,8 @@ class _MyAppState extends State<MyApp> {
                             return Text('Error: ${snapshot.error}');
                           } else {
                             // Not connected - go to Login
-                            if (true){
-                              return LoginMoreDetails();
+                            if (snapshot.data == ""){
+                              return Login();
                             }
                             // Connected - update current_user and go to home page
                             else {
